8d6407a6ecd79ac97e99b99186829309a2e86975
Justin Rosenthal <justin.rosenthal@gmail.com>
2022-02-24 12:45:06 -0800

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # dispersed-network-cli
+
+A command line tool for working with the Dispersed Network.
+
+## Installation
+
+```sh
+npm install dispersed-network
+```
+
+## Usage
+
+### Generating a Manifest
+
+Once a site has been built and is ready for bundling, run the following command to generate the Dispersed Network manifest:
+```sh
+npx dispersed-network generate-manifest <build-dir>
+```
+The manifest will automatically be saved as `<build-dir>/ngsw.json`.
---
+++ cli.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+const { exec } = require('child_process');
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
+
+yargs(hideBin(process.argv))
+  .command(
+    'generate-manifest <build-dir>',
+    'Generate a new site manifest',
+    (yargs) => {
+        return yargs
+        .positional('build-dir', {
+            describe: 'Relative path to the build directory (e.g. ./dist)',
+        })
+    },
+    generateManifest
+  )
+  .demandCommand(1)
+  .parse();
+
+function generateManifest(argv) {
+    const buildDir = argv['build-dir'];
+    const rootDir = './node_modules/dispersed-network';
+    exec(`${rootDir}/node_modules/.bin/ngsw-config ${buildDir} ${rootDir}/ngsw-config.json`, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return;
+        }
+        console.log(`Successfully wrote ${buildDir}/ngsw.json`);
+    });
+}
